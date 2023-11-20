283671cd636d12e55b624b7b1c26cdeacbbbe377
Lucas Álek <lucas.maistro@gmail.com>
2023-11-20 20:10:34 -0300

    chore: app clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addMovie } from "./features/moviesSlice";
 
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   const movies = useSelector((state) => state.movies);
   const dispatch = useDispatch();
 
@@ -16,36 +9,7 @@ function App() {
     dispatch(addMovie({ name: "new movie", genre: "terror", duration: 120 }));
   };
 
-  console.log(movies);
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleClick}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        {movies.map((movie) => {
-          return (
-            <>
-              <p>{movie.name}</p>
-            </>
-          );
-        })}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
